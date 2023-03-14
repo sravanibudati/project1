81d8e73b3714296bc3670c8e3e2b113e9aa4e1b5
sravanibudati <sravani.budati@gmail.com>
2023-03-14 22:16:14 +0530

    second comit

--- Myntra/src/Login/Dog.java
+++ Myntra/src/Login/Dog.java
@@ -5,8 +5,8 @@ public class Dog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		LoginTwo log = new LoginTwo();
+		System.out.println("Checking for git");
 	}
 
 }
